477ea2618f04f700f42b77ece049b4e445decc0d
Tomaž Jakša <jaksa.tomaz@gmail.com>
2021-03-13 23:12:21 +0100

    Naloga dokončana.

---
+++ Creatim Naloga/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Creatim_Naloga.Models;
+
+namespace Creatim_Naloga.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult NewProducts()
+        {
+            //kliči View Component New Products.. HTML koda v Views > Shared > Components > New ProductsVC
+            // kot parametre pošljemo čez število izdelkov, ki jih želimo izpisati in ali želimo aktivirati infinite scroll
+            // ta komponenta se kliče kadar ne spreminjamo filtra. Torej, ko scrollamo do dna strani, ta ViewComponent izpiše 4 nove izdelke, v neskončnost
+            return ViewComponent(nameof(Creatim_Naloga.ViewComponents.ViewComponents.NewProductsVC), new { nOfProducts = 4, infiniteScroll=true });
+        }
+
+        public IActionResult SimulateProducts(int nOfProducts)
+        {
+            return ViewComponent(nameof(Creatim_Naloga.ViewComponents.ViewComponents.NewProductsVC), new { nOfProducts= nOfProducts, infiniteScroll=false });
+        }
+    }
+}
---
+++ Creatim Naloga/ViewComponents/NewProductsViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creatim_Naloga.ViewComponents
+{
+    public enum ViewComponents
+    {
+        NewProductsVC
+    }
+
+    [ViewComponent(Name = nameof(ViewComponents.NewProductsVC))]
+    public class NewProductsViewComponent: ViewComponent
+    {
+        
+        public NewProductsViewComponent() { 
+        
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int nOfProducts, bool infiniteScroll) {
+
+            //podamo čez število izdelkov, ki ga želimo izpisati v View-u
+            ViewData["NumberOfProducts"] = nOfProducts;
+            
+            // true ali false vrednost ali se naj izpisuje infinite scroll
+            ViewData["Infinite"] = infiniteScroll;
+            return View();
+        }
+    }
+
+}
